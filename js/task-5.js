46f6f476a61f0780450fca978e7a5ab9177c4d15
Nataliya_Yavtushenko <yavtushenkon@gmail.com>
2024-10-05 21:15:02 +0300

    task36

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+
+  spanColor.textContent = newColor;
+});
