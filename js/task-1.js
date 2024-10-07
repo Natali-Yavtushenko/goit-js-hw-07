cf440d79eb59aec2ce8ecec04655e2f25771052f
Nataliya_Yavtushenko <yavtushenkon@gmail.com>
2024-10-07 15:15:23 +0300

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -2,6 +2,6 @@ const categoriesList = document.querySelectorAll("#categories .item");
 categoriesList.forEach((category) => {
   const categoryTitle = category.querySelector("h2").textContent;
   const categoryItems = category.querySelectorAll("ul li").length;
-
- 
+  console.log(categoryTitle);
+  console.log(categoryItems);
 });
